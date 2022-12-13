cec75f28e93b9f81851517ddcd2ebd93524b377b
Eda <edaalbayrak86@gmail.com>
2022-12-14 01:13:46 +0300

    project

---
+++ src/schoolAdministrationProject/Person.java
@@ -0,0 +1,15 @@
+package schoolAdministrationProject;
+
+public class Person {
+    int ID ;
+    int age;
+    String name;
+    String surname;
+
+    public Person(int ID, int age, String name, String surname) {
+        this.ID = ID;
+        this.age = age;
+        this.name = name;
+        this.surname = surname;
+    }
+}
---
+++ src/schoolAdministrationProject/Runner.java
@@ -0,0 +1,9 @@
+package schoolAdministrationProject;
+
+public class Runner {
+    public static void main(String[] args) {
+
+        Process.mainMenu();
+
+    }
+}
